0c388de280af82150a4637816288f95aa1fbf6e1
Kyrylo Sulihan <paulhighrow@gmail.com>
2023-01-16 14:28:08 +0200

    Done

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,1 +1,14 @@
-export const SectionTitle = ""
+import PropTypes from 'prop-types';
+import { Section, Title } from './SectionTitle.styled';
+
+export const SectionTitle = ({ title, children }) => (
+  <Section>
+    <Title>{title}</Title>
+    {children}
+  </Section>
+);
+
+SectionTitle.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
